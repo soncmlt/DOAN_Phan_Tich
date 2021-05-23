9d1f42e43b053d19c6209a3d4a0365617a610b46
Bao_Son <soncmlt@gmail.com>
2021-05-23 22:55:58 +0700

    xuly nhan vien

---
+++ DEV_PhanTIch/DEV_PhanTIch/XuLiNhanVien.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEV_PhanTIch
+{
+    public class XuLiNhanVien
+    {
+        DataThuocDataContext data = new DataThuocDataContext();
+
+
+        public IQueryable<NhanVien> loadNV()
+        {
+            return data.NhanViens.Select(t => t);
+        }
+
+
+        public bool ThemNV(String pmaNV, String phoTen, String pgioitinh, int ptuoi, String pdiaChi, string pchucvu, String pphanQuyen, String ptenDN, String pMK)
+        {
+
+        }
+    }
+}
