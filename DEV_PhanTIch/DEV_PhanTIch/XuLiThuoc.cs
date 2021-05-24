484f4c8e1caea715d2f637674f2e51fe588e97d5
Bao_Son <soncmlt@gmail.com>
2021-05-24 13:57:53 +0700

    Khasch Hangf

---
+++ DEV_PhanTIch/DEV_PhanTIch/XuLiThuoc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEV_PhanTIch
+{
+    public class XuLiThuoc
+    {
+
+        DataThuocDataContext data = new DataThuocDataContext();
+
+        public IQueryable<Thuoc> loadThuoc()
+        {
+            return data.Thuocs.Select(t => t);
+        }
+    }
+}
