d7489b582d5ae0b19aa2d2ba50258fcb4371bca7
Bao_Son <soncmlt@gmail.com>
2021-05-24 14:54:23 +0700

    Tìm Kiếm Thuốc

--- DEV_PhanTIch/DEV_PhanTIch/frmQLThuoc.cs
+++ DEV_PhanTIch/DEV_PhanTIch/frmQLThuoc.cs
@@ -23,5 +23,26 @@ namespace DEV_PhanTIch
         {
             gridDsThuoc.DataSource = thuoc.loadThuoc();
         }
+
+        private void txtThongTin_TextChanged(object sender, EventArgs e)
+        {
+            BindingSource bs = new BindingSource();
+            bs.DataSource = gridDsThuoc.DataSource;
+            if (cbPhuongThuc.SelectedItem.ToString() == "Mã Thuốc")
+            {
+                bs.Filter = "ma_thuoc like '%" + txtThongTin.Text + "%'";
+                gridDsThuoc.DataSource = bs;
+            }
+            else if (cbPhuongThuc.SelectedItem.ToString() == "Mã Nhóm")
+            {
+                bs.Filter = "ma_nhom like '%" + txtThongTin.Text + "%'";
+                gridDsThuoc.DataSource = bs;
+            }
+            else if (cbPhuongThuc.SelectedItem.ToString() == "Tên Thuốc")
+            {
+                bs.Filter = "ten_thuoc like '%" + txtThongTin.Text + "%'";
+                gridDsThuoc.DataSource = bs;
+            }
+        }
     }
 }
